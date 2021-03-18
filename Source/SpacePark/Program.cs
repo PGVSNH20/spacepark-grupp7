f358a193d60210b9b29488822350f449bfda86fd
Rvonkrusenstjerna <70213477+Rvonkrusenstjerna@users.noreply.github.com>
2021-03-18 14:26:07 +0100

    Added Project
    Loop

---
+++ Source/SpacePark/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SpacePark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("StarWars");
+            }
+
+
+
+        }
+    }
+}
