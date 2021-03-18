cafc2ad52e008936914591327f53f98648b49953
Rvonkrusenstjerna <70213477+Rvonkrusenstjerna@users.noreply.github.com>
2021-03-18 14:54:53 +0100

    Added Logic to Parking

---
+++ Source/SpacePark/Classes/ParkingLot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePark.Classes
+{
+    class ParkingLot
+    {
+        List<ParkingSpace> spaces = new List<ParkingSpace>();
+
+
+
+        public bool full()
+        {
+            foreach (ParkingSpace item in spaces)
+            {
+
+                if (item.occupied == false)
+                {
+                    return false;
+                }
+
+                
+
+            }
+
+            return true;
+        }
+
+
+    }
+}
