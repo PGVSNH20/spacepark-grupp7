08db6637d386b0a0f2d99237a7cd585266769174
Rvonkrusenstjerna <70213477+Rvonkrusenstjerna@users.noreply.github.com>
2021-03-19 14:01:19 +0100

    Made a Vehicle Class
    Moved incommon variables from Vehicle and Starship Class to a BaseVehicle Class

--- Source/SpacePark/Types/Starship.cs
+++ Source/SpacePark/Types/Starship.cs
@@ -6,26 +6,12 @@ using System.Threading.Tasks;
 
 namespace SpacePark.Types
 {
-    public class StarshipData
+    public class StarshipData : BaseVehicle
     {
-        public string Name { get; set; }
-        public string Model { get; set; }
-        public string Manufacturer { get; set; }
-        public string CostInCredits { get; set; }
-        public string Length { get; set; }
-        public string MaxAtmosphericSpeed { get; set; }
-        public string Crew { get; set; }
-        public string Passengers { get; set; }
-        public string Cargo_capacity { get; set; }
-        public string Consumables { get; set; }
         public string HyperdriveRating { get; set; }
         public string MGLT { get; set; }
         public string StarshipClass { get; set; }
-        public IList<string> Pilots { get; set; }
-        public IList<string> Films { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime Edited { get; set; }
-        public string URL { get; set; }
+        
     }
 
     public class Starship
