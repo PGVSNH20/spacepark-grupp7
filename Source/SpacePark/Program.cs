5c5e31b35848821565fc1f009833dd2ff3c06dd6
Kire <70213370+kireyperez@users.noreply.github.com>
2021-03-25 00:18:29 +0100

    Oops.

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -8,7 +8,21 @@ namespace SpacePark
 {
     class Program
     {
-           
+        /*
+       * Program flow:
+       * 1. User starts the program
+       * 2. Program checks to see if there are any free parking spaces. If true continue.
+     ✔️* 3. Program asks user to input their name.
+     ✔️* 4. Program fetches list of allowed users, and compares the name given to the list. If true, continue.
+          Additionally: Create a simple table in [the database] using Entity Framework and register that the user has registered
+          Also: List in the UI all completed registrations
+     ✔️* 5. Program asks uses to input name of ship.
+     ✔️* 6. Program fetches list of ships and checks if the ship is on the list of approved starships. If true, continue.
+       * 7. Program checks if there is a parking space of the appropriate size available in the parking lot. If true, continue.
+       * 8. Program gives a message to the user that parking has been allowed, says the ID of the parking space, and the expected cost of parking.
+       *  Also: Add a simple menu system if time allows.
+       * And done.
+       */
         static void Main(string[] args)
         {
             /*
