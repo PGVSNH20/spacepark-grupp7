35a7a8bc358c4173b22c21a34ec662941f3bf6e2
Martin Berntson <70233350+martinberntson@users.noreply.github.com>
2021-03-29 13:18:34 +0200

    La till EntityFramework NuGets, skapade en Context-klass och en Modell att basera databasen på.

---
+++ Source/SpacePark/Data/EFContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpacePark.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePark.Data
+{
+    class EFContext : DbContext
+    {
+        public DbSet<UserRegistrationEntry> Entries { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UserRegistry;");
+        }
+    }
+}
---
+++ Source/SpacePark/Models/UserRegistrationEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePark.Models
+{
+    class UserRegistrationEntry
+    {
+        public string Name { get; set; }
+        public DateTime TimeOfRegistration { get; set; }
+        public string ShipName { get; set; }
+        public int ParkingSpace { get; set; }
+        public double ParkingFee { get; set; }
+    }
+}
